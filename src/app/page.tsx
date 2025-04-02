347ab2f40ef47b60f4bf3e97ef5777544ea5f0d8
Karthik Kishore B <karthikkishore2603@gmail.com>
2025-04-02 16:49:27 +0530

    test vercel error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import About from '@/components/About';
 import Trust from '@/components/Trust';
 import Services from '@/components/Services';
 import Count2 from '@/components/Count2';
-import Testimonial from '@/components/Testimonial';
+// import Testimonial from '@/components/Testimonial';
 import Contact from '@/components/Contact';
 import Editor from '@/components/Editor';
 import Footer from '@/components/Footer';
